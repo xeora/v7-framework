c608ff9e6d935b421333c6438023ad465b036ce4
Tuna Celik <admin@xeora.org>
2017-12-07 02:55:46 +0100

    Xeora CLI .NET Core development

---
+++ CLI/Core/Xeora.CLI/Basics/Framework.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Xeora.CLI.Basics
+{
+    public class Framework : ICommand
+    {
+        private const string XEORAORG = "http://www.xeora.org/Releases/v7";
+
+        private string[] _AvailArchitectures = { "x86", "x64" };
+        private string[] _AvailPlatforms = { "core", "standard" };
+
+        private string _XeoraFrameworkPath;
+        private string _Architecture;
+        private string _Platform;
+        private string _Release;
+
+        private long _LastPercent;
+
+        public Framework()
+        {
+            this._Architecture = "x64";
+            this._Platform = "core";
+            this._Release = "latest";
+        }
+
+        public void PrintUsage()
+        {
+            Console.WriteLine();
+            Console.WriteLine("xeora framework OPTIONS");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            Console.WriteLine("   -h, --help                  print this screen");
+            Console.WriteLine("   -f, --framework PATH        xeora framework installation path (required)");
+            Console.WriteLine("   -a, --arch (x86|x64)        specify the target CPU architecture otherwise it will be 'x64'");
+            Console.WriteLine("   -p, --platf (standard|core) platform specific xeora release otherwise it will download 'core' release");
+            Console.WriteLine("   -r, --release VERSION       downloads the specified xeora framework version otherwise it will download 'latest' release");
+            Console.WriteLine();
+        }
+
+        public int SetArguments(string[] args)
+        {
+            for (int aC = 0; aC < args.Length; aC++)
+            {
+                switch (args[aC])
+                {
+                    case "-h":
+                    case "--help":
+                        this.PrintUsage();
+                        return -1;
+                    case "-f":
+                    case "--framework":
+                        if (!this.CheckArgument(args, aC))
+                        {
+                            this.PrintUsage();
+                            Console.WriteLine("xeora framework installation path should be specified");
+                            Console.WriteLine();
+                            return 2;
+                        }
+                        this._XeoraFrameworkPath = args[aC + 1];
+                        aC++;
+
+                        break;
+                    case "-a":
+                    case "--arch":
+                        if (!this.CheckArgument(args, aC))
+                        {
+                            this.PrintUsage();
+                            Console.WriteLine("CPU architecture should be specified");
+                            Console.WriteLine();
+                            return 2;
+                        }
+                        this._Architecture = args[aC + 1];
+                        if (Array.IndexOf<string>(this._AvailArchitectures, this._Architecture) == -1)
+                        {
+                            this.PrintUsage();
+                            Console.WriteLine("CPU architecture is not recognized");
+                            Console.WriteLine();
+                            return 2;
+                        }
+                        aC++;
+
+                        break;
+                    case "-p":
+                    case "--platf":
+                        if (!this.CheckArgument(args, aC))
+                        {
+                            this.PrintUsage();
+                            Console.WriteLine("platform should be specified");
+                            Console.WriteLine();
+                            return 2;
+                        }
+                        this._Platform = args[aC + 1];
+                        if (Array.IndexOf<string>(this._AvailPlatforms, this._Platform) == -1)
+                        {
+                            this.PrintUsage();
+                            Console.WriteLine("platform is not recognized");
+                            Console.WriteLine();
+                            return 2;
+                        }
+                        aC++;
+
+                        break;
+                    case "-r":
+                    case "--release":
+                        if (!this.CheckArgument(args, aC))
+                        {
+                            this.PrintUsage();
+                            Console.WriteLine("xeora framework version should be specified");
+                            Console.WriteLine();
+                            return 2;
+                        }
+                        this._Release = args[aC + 1];
+                        aC++;
+
+                        break;
+                }
+            }
+
+            if (string.IsNullOrEmpty(this._XeoraFrameworkPath))
+            {
+                this.PrintUsage();
+                Console.WriteLine("xeora framework installation path is required");
+                Console.WriteLine();
+                return 2;
+            }
+
+            return 0;
+        }
+
+        public int Execute()
+        {
+            try
+            {
+                string requestURL = Framework.XEORAORG;
+                requestURL = string.Concat(requestURL, "/", (this._Architecture == "x86" ? "Any" : this._Architecture), "/", this._Platform, "_v", this._Release, "_", this._Architecture, ".zip");
+
+                HttpClient client = new HttpClient();
+                client.Timeout = new TimeSpan(0, 0, 15);
+                Task<HttpResponseMessage> task = client.GetAsync(requestURL);
+                task.Wait();
+
+                if (!task.Result.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("release has not been found");
+                    return 2;
+                }
+
+                DirectoryInfo xeoraFrameworkRoot =
+                    new DirectoryInfo(this._XeoraFrameworkPath);
+                if (!xeoraFrameworkRoot.Exists)
+                    xeoraFrameworkRoot.Create();
+                Console.WriteLine(string.Format("Downloading Xeora Framework to {0}", xeoraFrameworkRoot.FullName));
+
+                this._LastPercent = -1;
+                this.DownloadAsync(xeoraFrameworkRoot, task.Result).Wait();
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                return 0;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("communication problem with server");
+                return 1;
+            }
+        }
+
+        private bool CheckArgument(string[] argument, int index)
+        {
+            if (argument.Length <= index + 1)
+                return false;
+
+            string value = argument[index + 1];
+            if (value.IndexOf("-") == 0)
+                return false;
+
+            return true;
+        }
+
+        private async Task DownloadAsync(DirectoryInfo target, HttpResponseMessage responseMessage)
+        {
+            FileInfo targetFile = 
+                new FileInfo(Path.Combine(target.FullName, "xeora.release.zip"));
+            if (targetFile.Exists)
+                targetFile.Delete();
+            
+            long total = responseMessage.Content.Headers.ContentLength.HasValue ? responseMessage.Content.Headers.ContentLength.Value : -1L;
+
+            Stream remoteFS = null;
+            Stream targetFS = null;
+            try
+            {
+                targetFS = targetFile.OpenWrite();
+                remoteFS = await responseMessage.Content.ReadAsStreamAsync();
+
+                long totalRead = 0L;
+                var buffer = new byte[8192];
+
+                do
+                {
+                    int bR = await remoteFS.ReadAsync(buffer, 0, buffer.Length);
+
+                    if (bR == 0)
+                        break;
+
+                    targetFS.Write(buffer, 0, bR);
+
+                    totalRead += bR;
+
+                    this.UpdateProgress(totalRead, total);
+                } while (true);
+            }
+            finally
+            {
+                if (remoteFS != null)
+                    remoteFS.Close();
+
+                if (targetFS != null)
+                    targetFS.Close();
+            }
+
+            ZipFile.ExtractToDirectory(targetFile.FullName, target.FullName);
+            targetFile.Delete();
+        }
+
+        private void UpdateProgress(long current, long total)
+        {
+            if (total == -1)
+            {
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write("\t Downloading... ");
+
+                return;
+            }
+
+            long percent = (current * 100) / total;
+
+            if (this._LastPercent != percent)
+            {
+                this._LastPercent = percent;
+
+                int curPos = Console.CursorLeft;
+
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write("\t Downloading... ");
+                Console.Write(percent.ToString().PadLeft(3));
+                Console.Write("% ");
+                for (int pC = 0; pC < this._LastPercent; pC += 2)
+                    Console.Write("*");
+                if (this._LastPercent == 100)
+                    Console.Write(" Completed");
+            }
+        }
+    }
+}
---
+++ CLI/Core/Xeora.CLI/Tools/CompileHelper.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Xeora.Extension.Tools;
+
+namespace Xeora.CLI.Tools
+{
+    public class CompileHelper
+    {
+        private int _LastPercent;
+
+        public void Compile(string projectRoot, string[] domainPath, string password, string output, bool recursive)
+        {
+            if (domainPath == null)
+                domainPath = new string[] { };
+            
+            if (domainPath.Length > 0)
+            {
+                DomainCompilerInfo domainCompilerInfo =
+                    new DomainCompilerInfo(projectRoot, domainPath, password, output);
+
+                this.Compile(domainCompilerInfo);
+
+                if (!recursive)
+                    return;
+            }
+
+            string[] domains = this.GetSubDomains(projectRoot, domainPath);
+            foreach (string domain in domains)
+            {
+                string[] newDomainPath = new string[domainPath.Length + 1];
+                Array.Copy(domainPath, 0, newDomainPath, 0, domainPath.Length);
+                newDomainPath[newDomainPath.Length - 1] = domain;
+
+                this.Compile(projectRoot, newDomainPath, password, output, recursive);
+            }
+        }
+
+        private void Compile(DomainCompilerInfo domainCompilerInfo)
+        {
+            this._LastPercent = -1;
+
+            Console.WriteLine(string.Format("Compiling {0}", string.Join('\\', domainCompilerInfo.DomainPath)));
+
+            Compiler xeoraCompiler = new Compiler(domainCompilerInfo.DomainLocation);
+            xeoraCompiler.Progress += new Compiler.ProgressEventHandler(this.UpdateProgress);
+
+            domainCompilerInfo.RemoveTarget();
+            this.AddFiles(domainCompilerInfo.DomainLocation, ref xeoraCompiler);
+
+            Stream contentFS = null;
+            try
+            {
+                domainCompilerInfo.Setup();
+
+                contentFS = new FileStream(domainCompilerInfo.OutputFile, FileMode.Create, FileAccess.ReadWrite);
+                xeoraCompiler.CreateDomainFile(domainCompilerInfo.Password, ref contentFS);
+            }
+            finally
+            {
+                if (contentFS != null)
+                    contentFS.Close();
+
+                GC.SuppressFinalize(contentFS);
+            }
+
+            if (xeoraCompiler.PasswordHash != null)
+            {
+                Stream securedFS = null;
+                try
+                {
+                    domainCompilerInfo.Setup();
+
+                    securedFS = new FileStream(domainCompilerInfo.KeyFile, FileMode.Create, FileAccess.ReadWrite);
+                    securedFS.Write(xeoraCompiler.PasswordHash, 0, xeoraCompiler.PasswordHash.Length);
+                }
+                finally
+                {
+                    if (securedFS != null)
+                        securedFS.Close();
+
+                    GC.SuppressFinalize(securedFS);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private void AddFiles(string workingPath, ref Compiler xeoraCompilerObj)
+        {
+            foreach (string path in Directory.GetDirectories(workingPath))
+            {
+                DirectoryInfo DI = new DirectoryInfo(path);
+
+                if (string.Compare(DI.Name, "addons", true) != 0 &&
+                    string.Compare(DI.Name, "executables", true) != 0)
+                    this.AddFiles(path, ref xeoraCompilerObj);
+            }
+
+            foreach (string file in Directory.GetFiles(workingPath))
+                xeoraCompilerObj.AddFile(file);
+        }
+
+        private void UpdateProgress(int current, int total)
+        {
+            int percent = (current * 100) / total;
+
+            if (this._LastPercent != percent)
+            {
+                this._LastPercent = percent;
+
+                int curPos = Console.CursorLeft;
+
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write('\t');
+                Console.Write(percent.ToString().PadLeft(3));
+                Console.Write("% ");
+                for (int pC = 0; pC < this._LastPercent; pC++)
+                    Console.Write("*");
+                if (this._LastPercent == 100)
+                    Console.Write(" Completed");
+            }
+        }
+
+        private string[] GetSubDomains(string projectRoot, string[] domainPath)
+        {
+            if (string.IsNullOrEmpty(projectRoot))
+                return new string[] { };
+
+            string domainLocation = Path.Combine(projectRoot, "Domains");
+
+            if (domainPath == null)
+                domainPath = new string[] { };
+
+            if (domainPath.Length > 0)
+            {
+                domainLocation = Path.Combine(domainLocation, domainPath[0], "Addons");
+                for (int pC = 1; pC < domainPath.Length; pC++)
+                    domainLocation = Path.Combine(domainLocation, domainPath[pC], "Addons");
+            }
+
+            DirectoryInfo domainRoot = new DirectoryInfo(domainLocation);
+
+            if (!domainRoot.Exists)
+                return new string[] { };
+
+            List<string> domains = new List<string>();
+
+            foreach (DirectoryInfo domain in domainRoot.GetDirectories())
+                domains.Add(domain.Name);
+
+            return domains.ToArray();
+        }
+
+        private class DomainCompilerInfo
+        {
+            public DomainCompilerInfo(string projectRoot, string[] domainPath, string password, string outputPath)
+            {
+                this.ProjectRoot = projectRoot;
+                this.DomainPath = domainPath;
+                this.DomainLocation = this.MakePath(projectRoot, domainPath, false);
+                this.Password = password;
+
+                this.OutputPath = this.DomainLocation;
+                if (!string.IsNullOrEmpty(outputPath))
+                    this.OutputPath = this.MakePath(outputPath, domainPath, true);
+
+                this.OutputFile = Path.Combine(this.OutputPath, "Content.xeora");
+                this.KeyFile = Path.Combine(this.OutputPath, "Content.secure");
+            }
+
+            public string ProjectRoot { get; private set; }
+            public string[] DomainPath { get; private set; }
+            public string DomainLocation { get; private set; }
+            public string Password { get; private set; }
+
+            private string OutputPath { get; set; }
+            public string OutputFile { get; private set; }
+            public string KeyFile { get; private set; }
+
+            private string MakePath(string projectRoot, string[] domainPath, bool omitDomainsFolder)
+            {
+                string path = projectRoot;
+                if (!omitDomainsFolder)
+                    path = Path.Combine(path, "Domains");
+                path = Path.Combine(path, domainPath[0]);
+
+                for (int pC = 1; pC < domainPath.Length; pC++)
+                    path = Path.Combine(path, "Addons", domainPath[pC]);
+
+                return path;
+            }
+
+            public void Setup()
+            {
+                if (!Directory.Exists(this.OutputPath))
+                    Directory.CreateDirectory(this.OutputPath);
+            }
+
+            public void RemoveTarget()
+            {
+                try
+                {
+                    File.Delete(this.OutputFile);
+                }
+                catch (Exception)
+                { /* Just Handle Exceptions*/ }
+
+                try
+                {
+                    File.Delete(this.KeyFile);
+                }
+                catch (Exception)
+                { /* Just Handle Exceptions*/ }
+            }
+        }
+    }
+}
