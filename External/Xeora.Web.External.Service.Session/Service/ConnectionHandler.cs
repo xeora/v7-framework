ff8ac9a8e910e72f1d9087679d6fd3ec14ef315e
Tuna Celik <admin@xeora.org>
2017-12-09 15:17:13 +0100

    Shared Session (External) Service is implemented.

---
+++ External/Xeora.Web.External.Service.Session/Service/ConnectionHandler.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Xeora.Web.External.Service.Session
+{
+    public class ConnectionHandler
+    {
+        private TcpClient _RemoteClient;
+        private Stream _RemoteStream;
+        private MessageHandler _MessageHandler;
+
+        public ConnectionHandler(ref TcpClient remoteClient)
+        {
+            this._RemoteClient = remoteClient;
+            this._RemoteStream = this._RemoteClient.GetStream();
+            this._MessageHandler = new MessageHandler(ref this._RemoteStream);
+        }
+
+        public async void HandleAsync()
+        {
+            await Task.Run(() => this.ReadSocket());
+
+            this._RemoteClient.Close();
+        }
+
+        private void ReadSocket()
+        {
+            byte[] buffer = new byte[1024];
+            int bR = 0;
+
+            Stream contentStream = null;
+            try
+            {
+                this.MakeContentStream(ref contentStream);
+
+                do
+                {
+                    bR = this._RemoteStream.Read(buffer, 0, buffer.Length);
+                    if (bR == 0)
+                    {
+                        // give time to fill buffer
+                        System.Threading.Thread.Sleep(1);
+
+                        continue;
+                    }
+
+                    if (bR == 1 && contentStream.Length == 0 && buffer[0] == 0)
+                        return;
+
+                    contentStream.Seek(0, SeekOrigin.End);
+                    contentStream.Write(buffer, 0, bR);
+
+                    if (buffer[bR - 1] == 0)
+                    {
+                        // Process Message
+                        this._MessageHandler.Process(ref contentStream);
+
+                        this.MakeContentStream(ref contentStream);
+                    }
+                } while (true);
+            }
+            catch (System.Exception ex)
+            {
+                // Skip SocketExceptions
+                if (ex is IOException && ex.InnerException is SocketException)
+                    return;
+
+                Basics.Console.Push("SYSTEM ERROR", ex.Message, false);
+            }
+            finally
+            {
+                this.KillContentStream(ref contentStream);
+            }
+        }
+
+        private void MakeContentStream(ref Stream contentStream)
+        {
+            this.KillContentStream(ref contentStream);
+
+            contentStream = new MemoryStream();
+        }
+
+        private void KillContentStream(ref Stream contentStream)
+        {
+            if (contentStream != null)
+            {
+                contentStream.Close();
+                GC.SuppressFinalize(contentStream);
+            }
+        }
+    }
+}
---
+++ External/Xeora.Web.External.Service.Session/Service/MessageHandler.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.IO;
+
+namespace Xeora.Web.External.Service.Session
+{
+    public class MessageHandler
+    {
+        private Stream _ResponseStream;
+        private Basics.Session.IHttpSession _HttpSession;
+
+        public MessageHandler(ref Stream responseStream)
+        {
+            this._ResponseStream = responseStream;
+            this._HttpSession = null;
+        }
+
+        public void Process(ref Stream requestStream)
+        {
+            requestStream.Seek(0, SeekOrigin.Begin);
+
+            BinaryReader binaryReader =
+                 new BinaryReader(requestStream);
+
+            char[] command = binaryReader.ReadChars(3);
+
+            switch (new string(command))
+            {
+                case "ACQ":
+                    this.HandleACQ(ref binaryReader);
+
+                    break;
+                case "GET":
+                    this.HandleGET(ref binaryReader);
+
+                    break;
+                case "SET":
+                    this.HandleSET(ref binaryReader);
+
+                    break;
+                case "KYS":
+                    this.HandleKYS(ref binaryReader);
+
+                    break;
+            }
+
+            // Consume Terminator
+            binaryReader.ReadByte();
+        }
+
+        private void HandleACQ(ref BinaryReader responseReader)
+        {
+            /*
+             * -> ACQ\SHORT\INT\0
+             * -> ACQ\SHORT\INT\BYTE\CHARS{BYTEVALUELENGTH}\0
+             */
+
+            short sessionTimeout = responseReader.ReadInt16();
+            int remoteIP = responseReader.ReadInt32();
+            byte sessionIDLength = responseReader.ReadByte();
+            string sessionID = string.Empty;
+
+            if (sessionIDLength > 0)
+                sessionID = new string(responseReader.ReadChars(sessionIDLength));
+
+            BinaryWriter binaryWriter = null;
+            Stream responseStream = null;
+
+            try
+            {
+                SessionManager.Current.Acquire(remoteIP, sessionID, sessionTimeout, out this._HttpSession);
+
+                responseStream = new MemoryStream();
+                binaryWriter = new BinaryWriter(responseStream);
+
+                /*
+                 * <- \BYTE\BYTE\CHARS{BYTEVALUELENGTH}\LONG\0
+                 */
+
+                binaryWriter.Write((byte)1);
+                binaryWriter.Write((byte)this._HttpSession.SessionID.Length);
+                binaryWriter.Write(this._HttpSession.SessionID.ToCharArray());
+                binaryWriter.Write(this._HttpSession.Expires.Ticks);
+                binaryWriter.Write((byte)0);
+                binaryWriter.Flush();
+
+                responseStream.Seek(0, SeekOrigin.Begin);
+                responseStream.CopyTo(this._ResponseStream);
+            }
+            catch
+            {
+                this._ResponseStream.Write(new byte[] { 2, 0 }, 0, 2);
+            }
+            finally
+            {
+                if (binaryWriter != null)
+                    binaryWriter.Close();
+
+                if (responseStream != null)
+                {
+                    responseStream.Close();
+                    GC.SuppressFinalize(responseStream);
+                }
+            }
+        }
+
+        private void HandleGET(ref BinaryReader responseReader)
+        {
+            /*
+             * -> GET\BYTE\CHARS{BYTEVALUELENGTH}\0
+             */
+
+            if (this._HttpSession == null)
+                return;
+
+            byte keyLength = responseReader.ReadByte();
+            string key = new string(responseReader.ReadChars(keyLength));
+
+            if (string.IsNullOrEmpty(key))
+                return;
+
+            object value = this._HttpSession[key];
+
+            BinaryWriter binaryWriter = null;
+            Stream responseStream = null;
+
+            try
+            {
+                responseStream = new MemoryStream();
+                binaryWriter = new BinaryWriter(responseStream);
+
+                /*
+                 * <- \BYTE\CHARS{BYTEVALUELENGTH}\INTEGER\BYTES{INTEGERVALUELENGTH}\0
+                 */
+
+                binaryWriter.Write(keyLength);
+                binaryWriter.Write(key.ToCharArray());
+
+                if (value != null)
+                {
+                    byte[] valueBytes = (byte[])value;
+
+                    binaryWriter.Write(valueBytes.Length);
+                    binaryWriter.Write(valueBytes, 0, valueBytes.Length);
+                }
+                else
+                    binaryWriter.Write((int)0);
+                binaryWriter.Write((byte)0);
+                binaryWriter.Flush();
+
+                responseStream.Seek(0, SeekOrigin.Begin);
+                responseStream.CopyTo(this._ResponseStream);
+            }
+            catch
+            {
+                return;
+            }
+            finally
+            {
+                if (binaryWriter != null)
+                    binaryWriter.Close();
+
+                if (responseStream != null)
+                {
+                    responseStream.Close();
+                    GC.SuppressFinalize(responseStream);
+                }
+            }
+        }
+
+        private void HandleSET(ref BinaryReader responseReader)
+        {
+            /*
+             * -> SET\BYTE\CHARS{BYTEVALUELENGTH}\INTEGER\BYTES{INTEGERVALUELENGTH}\0
+             */
+
+            if (this._HttpSession == null)
+                return;
+
+            byte keyLength = responseReader.ReadByte();
+            string key = new string(responseReader.ReadChars(keyLength));
+
+            int valueLength = responseReader.ReadInt32();
+            byte[] valueBytes = responseReader.ReadBytes(valueLength);
+
+            if (string.IsNullOrEmpty(key))
+                return;
+
+            this._HttpSession[key] = valueBytes;
+
+            /*
+             * <- \BYTE\0
+             */
+
+            this._ResponseStream.Write(new byte[] { 1, 0 }, 0, 2);
+        }
+
+        private void HandleKYS(ref BinaryReader responseReader)
+        {
+            if (this._HttpSession == null)
+                return;
+
+            BinaryWriter binaryWriter = null;
+            Stream responseStream = null;
+
+            try
+            {
+                responseStream = new MemoryStream();
+                binaryWriter = new BinaryWriter(responseStream);
+
+                /*
+                 * <- [KEY][KEY][KEY]...\0
+                 */
+
+                foreach (string key in this._HttpSession.Keys)
+                {
+                    binaryWriter.Write((byte)key.Length);
+                    binaryWriter.Write(key.ToCharArray());
+                }
+                binaryWriter.Write((byte)0);
+                binaryWriter.Flush();
+
+                responseStream.Seek(0, SeekOrigin.Begin);
+                responseStream.CopyTo(this._ResponseStream);
+            }
+            catch 
+            {
+                this._ResponseStream.Write(new byte[] { 0 }, 0, 1);
+            }
+            finally
+            {
+                if (binaryWriter != null)
+                    binaryWriter.Close();
+
+                if (responseStream != null)
+                {
+                    responseStream.Close();
+                    GC.SuppressFinalize(responseStream);
+                }
+            }
+        }
+    }
+}
