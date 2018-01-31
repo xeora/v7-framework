7f2cfe7c3ea06076a4eed9227e27724663b0c9cf
Tuna Celik <tuna@jakpark.com>
2018-01-31 17:04:44 +0100

    Condition accepts boolean input.

--- src/Xeora.Web.Basics/ControlResult/Conditional.cs
+++ src/Xeora.Web.Basics/ControlResult/Conditional.cs
@@ -15,6 +15,9 @@ namespace Xeora.Web.Basics.ControlResult
         public Conditional(Conditions result) =>
             this.Result = result;
 
+        public Conditional(bool result) =>
+            this.Result = result ? Conditions.True : Conditions.False;
+
         public Conditions Result { get; private set; }
     }
 }
