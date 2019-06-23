700b16750eb8273ef39d16a03cf73682c094c0c1
Tuna Celik <admin@xeora.org>
2019-06-23 14:46:56 +0200

    refactored ContentDescription

--- src/Xeora.Web/Global/ContentDescription.cs
+++ src/Xeora.Web/Global/ContentDescription.cs
@@ -22,49 +22,38 @@ namespace Xeora.Web.Global
             this.Parts = new List<string>();
             this.MessageTemplate = string.Empty;
 
-            // Parse Block Content
-            int firstContentIndex =
-                rawValue.IndexOf(":{", System.StringComparison.InvariantCulture);
+            int modifierBeginIndex = rawValue.LastIndexOf('~');
+            if (modifierBeginIndex == -1) return;
 
-            if (firstContentIndex == -1)
-                return;
+            string modifier =
+                rawValue.Substring(modifierBeginIndex + 1);
+
+            // Parse Block Content
+            int firstContentEndIndex =
+                rawValue.IndexOf(string.Format("{0}:{{", modifier), System.StringComparison.InvariantCulture);
+            if (firstContentEndIndex == -1) return;
 
             string directiveIdentifier =
-                rawValue.Substring(0, firstContentIndex);
+                rawValue.Substring(0, firstContentEndIndex);
 
+            string blockContent = rawValue;
+            bool isSpecialDirective = false;
             int colonIndex = 
                 directiveIdentifier.IndexOf(':');
-            bool isSpecialDirective = false;
-            string blockContent;
 
-            if (colonIndex == -1)
-            {
-                // Special Directive such as PC, MB, XF, AG
-                blockContent = rawValue;
+            if (colonIndex == -1) // Special Directive such as PC, MB, XF, AG
                 isSpecialDirective = true;
-            }
-            else
+            else // Common Directive such as DirectiveType:DirectiveID
             {
-                // Common Directive such as DirectiveType:DirectiveID
-                blockContent =
-                    rawValue.Substring(colonIndex + 1);
+                blockContent = blockContent.Substring(colonIndex + 1);
+                firstContentEndIndex -= colonIndex + 2; // 2 = 1: make it length + 1: skip colon
             }
 
-            // Cleanup if there is parameters
-            int parameterBeginIndex = blockContent.IndexOf('(');
-            int parameterEndIndex = blockContent.IndexOf('~');
-            if (parameterBeginIndex > -1 && parameterEndIndex > -1 && 
-                parameterEndIndex > parameterBeginIndex && 
-                parameterEndIndex < firstContentIndex)
-                blockContent = blockContent.Remove(parameterBeginIndex, parameterEndIndex - parameterBeginIndex);
-
-            // Update First Content Index
-            firstContentIndex = 
-                blockContent.IndexOf(":{", System.StringComparison.InvariantCulture);
+            blockContent = this.CleanupParameters(firstContentEndIndex, blockContent);
 
             // ControlIDWithIndex is Like ControlID~INDEX
             string controlIDWithIndex = 
-                blockContent.Substring(0, firstContentIndex);
+                blockContent.Substring(0, blockContent.IndexOf(":{", System.StringComparison.InvariantCulture));
 
             string openingTag = string.Format("{0}:{{", controlIDWithIndex);
             string closingTag = string.Format("}}:{0}", controlIDWithIndex);
@@ -74,8 +63,7 @@ namespace Xeora.Web.Global
             int idxCoreContEnd =
                 blockContent.LastIndexOf(closingTag, blockContent.Length, System.StringComparison.InvariantCulture);
 
-            if (idxCoreContStart != openingTag.Length || 
-                idxCoreContEnd != (blockContent.Length - openingTag.Length))
+            if (idxCoreContStart != openingTag.Length || idxCoreContEnd != (blockContent.Length - openingTag.Length))
                 throw new Exception.ParseException();
 
             string coreContent = 
@@ -83,40 +71,51 @@ namespace Xeora.Web.Global
             if (isSpecialDirective)
                 coreContent = coreContent.Trim();
 
-            if (ContentDescription._PartsCache.TryGetValue(controlIDWithIndex, out PartCache partCache))
-            {
-                if (string.Compare(partCache.Content, coreContent) == 0)
-                {
-                    this.Parts = partCache.Parts;
-                    this.MessageTemplate = partCache.MessageTemplate;
-
-                    return;
-                }
-            }
+            if (this.TryCacheToPrepare(controlIDWithIndex, coreContent)) return;
 
             this.PrepareDesciption(coreContent, controlIDWithIndex, isSpecialDirective);
+        }
+
+        private string CleanupParameters(int firstContentEndIndex, string blockContent)
+        {
+            int parameterBeginIndex = blockContent.IndexOf('(');
+            if (parameterBeginIndex == -1) return blockContent;
+            
+            if (firstContentEndIndex < parameterBeginIndex)
+                return blockContent;
+
+            return blockContent.Remove(parameterBeginIndex, firstContentEndIndex - parameterBeginIndex);
+        }
+
+        private bool TryCacheToPrepare(string controlIDWithIndex, string coreContent)
+        {
+            if (!ContentDescription._PartsCache.TryGetValue(controlIDWithIndex, out PartCache partCache))
+                return false;
+
+            if (string.Compare(partCache.Content, coreContent) != 0)
+                return false;
+
+            this.MessageTemplate = partCache.MessageTemplate;
+            this.Parts = partCache.Parts;
+
+            return true;
         }
 
         private void PrepareDesciption(string content, string controlIDWithIndex, bool isSpecialDirective)
         {
             string searchString =
                 string.Format("}}:{0}:{{", controlIDWithIndex);
-            string contentPart;
             int sIdx, cIdx = 0;
 
             do
             {
                 sIdx = content.IndexOf(searchString, cIdx, System.StringComparison.InvariantCulture);
+                if (sIdx == -1)
+                    sIdx = content.Length;
 
-                if (sIdx > -1)
-                {
-                    contentPart = content.Substring(cIdx, sIdx - cIdx);
-
-                    // Set cIdx and Move Forward to Length of SearchString
-                    cIdx = sIdx + searchString.Length;
-                }
-                else
-                    contentPart = content.Substring(cIdx);
+                string contentPart =
+                    content.Substring(cIdx, sIdx - cIdx);
+                cIdx = sIdx + searchString.Length;
 
                 if (isSpecialDirective)
                     contentPart = contentPart.Trim();
@@ -127,28 +126,32 @@ namespace Xeora.Web.Global
                         throw new Exception.MultipleBlockException("Only One Message Template Block Allowed!");
 
                     this.MessageTemplate = contentPart.Substring(ContentDescription.MESSAGE_TEMPLATE_POINTER_TEXT.Length);
+
+                    continue;
                 }
-                else
-                    if (!string.IsNullOrEmpty(contentPart))
-                        this.Parts.Add(contentPart);
-            } while (sIdx != -1);
+
+                if (string.IsNullOrEmpty(contentPart)) continue;
+
+                this.Parts.Add(contentPart);
+            } while (sIdx != content.Length);
 
             if (!this.HasParts)
                 throw new Exception.EmptyBlockException();
 
-            // Cache Result
-            PartCache partCache = new PartCache
-            {
-                Content = content,
-                Parts = this.Parts,
-                MessageTemplate = this.MessageTemplate
-            };
-
-            ContentDescription._PartsCache.TryAdd(controlIDWithIndex, partCache);
+            ContentDescription._PartsCache.TryAdd(
+                controlIDWithIndex,
+                new PartCache
+                {
+                    Content = content,
+                    Parts = this.Parts,
+                    MessageTemplate = this.MessageTemplate
+                }
+            );
         }
 
         public List<string> Parts { get; private set; }
         public bool HasParts => this.Parts.Count > 0;
+
         public string MessageTemplate { get; private set; }
         public bool HasMessageTemplate => !string.IsNullOrEmpty(this.MessageTemplate);
     }
