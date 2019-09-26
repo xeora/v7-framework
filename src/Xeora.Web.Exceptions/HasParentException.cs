bbf175678c856774fd667cebee2ebb2b0d5beaf7
Tuna Celik <admin@xeora.org>
2019-09-26 13:53:07 +0200

    removed unused exceptions

--- src/Xeora.Web.Exceptions/HasParentException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Xeora.Web.Exceptions
-{
-    public class HasParentException : System.Exception
-    {
-        public HasParentException() : 
-            base("Controller does not accept Parent!")
-        { }
-    }
-}
--- src/Xeora.Web.Exceptions/UnknownDirectiveException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Xeora.Web.Exceptions
-{
-    public class UnknownDirectiveException : System.Exception
-    {
-        public UnknownDirectiveException() : 
-            base("Directive pointer is not a known one!")
-        { }
-    }
-}
