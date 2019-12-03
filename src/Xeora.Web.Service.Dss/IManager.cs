de394d6b3adffdd30024ab7b27df8ea0cb877696
Tuna Celik <admin@xeora.org>
2019-12-03 02:07:49 +0100

    removed misnamed files and folders

--- src/Xeora.Web.Service.Dss/IManager.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Xeora.Web.Service.Dss
-{
-    public interface IManager
-    {
-        void Reserve(string uniqueId, short reservationTimeout, out Basics.Dss.IDss reservationObject);
-    }
-}
--- src/Xeora.Web.Service.Dss/IService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Xeora.Web.Service.Dss
-{
-    public interface IService
-    {
-        bool IsExpired { get; }
-        void Extend();
-    }
-}
