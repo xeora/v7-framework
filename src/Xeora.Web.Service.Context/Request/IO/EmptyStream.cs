219921e6b0e709d1055c6a507ce9820428ba75c8
Tuna Celik <tuna@jakpark.com>
2022-08-27 12:18:12 +0200

    Fixed empty content streaming

--- src/Xeora.Web.Service.Context/Request/IO/EmptyStream.cs
+++ src/Xeora.Web.Service.Context/Request/IO/EmptyStream.cs
@@ -6,7 +6,12 @@ namespace Xeora.Web.Service.Context.Request.IO
     public class EmptyStream : BodyStream
     {
         public override int Read(byte[] buffer, int offset, int count) => 0;
-        internal override ParserResultTypes ReadAllInto(ref Stream contentStream) => ParserResultTypes.Success;
+
+        internal override ParserResultTypes ReadAllInto(ref Stream contentStream)
+        {
+            contentStream ??= new MemoryStream();
+            return ParserResultTypes.Success;
+        }
         public override void Conclude() {}
 
         public override bool CanRead => true;
